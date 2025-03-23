e4f8b294c2db3122cd9ab48d0d6851c876717af7
sohamkawade <kawadesoham08@gmail.com>
2025-03-23 22:45:11 +0530

    Hello!

--- backend/app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-const contactRoutes = require('./routes/contactRoutes');
-
-// Routes
-app.use('/api/contact', contactRoutes);
-
-// ... rest of the code ... 
